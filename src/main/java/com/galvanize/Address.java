3d0ae7bbe4c870464458eb8a5b9a1865755884f5
Gordon H Graham <theboss@gordonhgraham.com>
2017-02-07 14:42:35 -0700

    add getters and setters to address class

--- src/main/java/com/galvanize/Address.java
+++ src/main/java/com/galvanize/Address.java
@@ -1,3 +1,9 @@
+package com.galvanize;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+
 public class Address {
   private String street;
   private String city;
@@ -18,6 +24,19 @@ public class Address {
 
   public String getZip() {
     return zip;
+  }
+
+  public void setStreet(String newStreet) {
+    street = newStreet;
+  }
+  public void setCity(String newCity) {
+    street = newCity;
+  }
+  public void setState(String newState) {
+    street = newState;
+  }
+  public void setZip(String newZip) {
+    street = newZip;
   }
 
 }
