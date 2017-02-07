98997708001c0830314bc7dd8cffa92382afb0b5
Gordon H Graham <theboss@gordonhgraham.com>
2017-02-07 14:06:30 -0700

    add address class

---
+++ src/main/java/com/galvanize/Address.java
@@ -0,0 +1,23 @@
+public class Address {
+  private String street;
+  private String city;
+  private String state;
+  private String zip;
+
+  public String getStreet() {
+    return street;
+  }
+
+  public String getCity() {
+    return city;
+  }
+
+  public String getState() {
+    return state;
+  }
+
+  public String getZip() {
+    return zip;
+  }
+
+}
